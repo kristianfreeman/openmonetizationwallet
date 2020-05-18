e7247e25d8a59bf2dc1e4e7be0c994cd12965946
Kristian Freeman <kristian@kristianfreeman.com>
2020-05-18 15:08:28 -0500

    Restore add wallet button

--- src/pages/admin/users.js
+++ src/pages/admin/users.js
@@ -227,6 +227,15 @@ const AdminUsersPage = () => {
                 Cancel
               </button>
             </span>
+            <span className="inline-flex rounded-md shadow-sm mr-2">
+              <button
+                onClick={addWallet}
+                type="button"
+                className="inline-flex items-center px-4 py-2 border border-gray-300 text-base leading-6 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150"
+              >
+                Add wallet
+              </button>
+            </span>
             <span className="inline-flex rounded-md shadow-sm">
               <button
                 onClick={save}
