5ec78afa366fc2812bce9ceb4f2f095a3da6c6d9
Kristian Freeman <kristian@kristianfreeman.com>
2020-05-18 14:33:39 -0500

    Potentially fix CSS not building correctly

--- postcss.config.js
+++ postcss.config.js
@@ -7,8 +7,8 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    ...process.env.NODE_ENV == 'production'
-      ? [purgecss]
-      : []
+    // ...process.env.NODE_ENV == 'production'
+    //   ? [purgecss]
+    //   : []
   ]
 }
